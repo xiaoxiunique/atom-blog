3025bc058515c6cec8e228d0116f872561cf1f3d
Your Name <you@example.com>
2020-06-26 18:34:38 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,13 @@
-const { fs, path } = require('@vuepress/shared-utils');
+const { fs, path } = require('@vuepress/shared-utils')
 
 // 读取blog 文件目录下的内容
 let listBlogFiles = fs
   .readdirSync(path.resolve(__dirname, '../blog/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listBlogFiles = [['', '深度好文'], ...listBlogFiles];
+listBlogFiles = [['', '深度好文'], ...listBlogFiles]
 
 function getBlogSidebarList() {
   return [
@@ -17,7 +17,7 @@ function getBlogSidebarList() {
       sidebarDepth: 3,
       children: listBlogFiles,
     },
-  ];
+  ]
 }
 
 // 读取idea 文件下的内容
@@ -25,9 +25,9 @@ let listIdeaFiles = fs
   .readdirSync(path.resolve(__dirname, '../idea/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listIdeaFiles = [['', '开源项目'], ...listIdeaFiles];
+listIdeaFiles = [['', '开源项目'], ...listIdeaFiles]
 
 function getIdeaSidebar() {
   return [
@@ -37,7 +37,7 @@ function getIdeaSidebar() {
       sidebarDepth: 3,
       children: listIdeaFiles,
     },
-  ];
+  ]
 }
 
 // 读取devops 文件下的内容
@@ -45,9 +45,9 @@ let listDevOpsFiles = fs
   .readdirSync(path.resolve(__dirname, '../devops/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listDevOpsFiles = [['', '介绍'], ...listDevOpsFiles];
+listDevOpsFiles = [['', '介绍'], ...listDevOpsFiles]
 
 function getDevOpsSidebar() {
   return [
@@ -57,7 +57,7 @@ function getDevOpsSidebar() {
       sidebarDepth: 3,
       children: listDevOpsFiles,
     },
-  ];
+  ]
 }
 
 // 读取web 文件下的内容
@@ -65,9 +65,9 @@ let listWebFiles = fs
   .readdirSync(path.resolve(__dirname, '../web/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listWebFiles = [['', '介绍'], ...listWebFiles];
+listWebFiles = [['', '介绍'], ...listWebFiles]
 
 function getWebSidebar() {
   return [
@@ -77,7 +77,7 @@ function getWebSidebar() {
       sidebarDepth: 3,
       children: listWebFiles,
     },
-  ];
+  ]
 }
 
 // 读取设计模式文件下的内容
@@ -85,9 +85,9 @@ let listDesignPattern = fs
   .readdirSync(path.resolve(__dirname, '../designPattern/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listDesignPattern = [['', '设计原则'], ...listDesignPattern];
+listDesignPattern = [['', '设计原则'], ...listDesignPattern]
 
 function getDesignSidebar() {
   return [
@@ -97,7 +97,7 @@ function getDesignSidebar() {
       sidebarDepth: 3,
       children: listDesignPattern,
     },
-  ];
+  ]
 }
 
 // 读取数据结构与算法文件下的内容
@@ -105,9 +105,9 @@ let listAlgorithm = fs
   .readdirSync(path.resolve(__dirname, '../algorithm/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listAlgorithm = [['', '算法概览'], ...listAlgorithm];
+listAlgorithm = [['', '算法概览'], ...listAlgorithm]
 
 function getAlgorithmSidebar() {
   return [
@@ -117,7 +117,7 @@ function getAlgorithmSidebar() {
       sidebarDepth: 3,
       children: listAlgorithm,
     },
-  ];
+  ]
 }
 
 // 读取java文件下的内容
@@ -125,9 +125,9 @@ let listjava = fs
   .readdirSync(path.resolve(__dirname, '../java/'))
   .filter((f) => f !== 'README.md')
   .map((filename) => [filename.slice(0, -3), filename.slice(0, -3)])
-  .sort();
+  .sort()
 
-listjava = [['', '知识预览'], ...listjava];
+listjava = [['', '知识预览'], ...listjava]
 
 function getjavaSidebar() {
   return [
@@ -137,7 +137,7 @@ function getjavaSidebar() {
       sidebarDepth: 3,
       children: listjava,
     },
-  ];
+  ]
 }
 
 module.exports = {
@@ -183,30 +183,5 @@ module.exports = {
         },
       },
     ],
-    [
-      'vuepress-plugin-helper-live2d',
-      {
-        live2d: {
-          // 是否启用(关闭请设置为false)(default: true)
-          enable: true,
-          // 模型名称(default: hibiki)>>>取值请参考：
-          // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-          model: 'wanko',
-          display: {
-            position: 'right', // 显示位置：left/right(default: 'right')
-            width: 135, // 模型的长度(default: 135)
-            height: 300, // 模型的高度(default: 300)
-            hOffset: 65, //  水平偏移(default: 65)
-            vOffset: 0, //  垂直偏移(default: 0)
-          },
-          mobile: {
-            show: true, // 是否在移动设备上显示(default: false)
-          },
-          react: {
-            opacity: 0.8, // 模型透明度(default: 0.8)
-          },
-        },
-      },
-    ],
   ],
-};
+}
