2b3a3344702e7f7c61aa23c136085b7fe3a8705c
xiaoxiunique@gmail.com <xiaoxiunique@gmail.com>
2020-09-28 15:52:16 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -205,6 +205,11 @@ module.exports = {
         },
       },
     ],
+    ['vuepress-plugin-baidu-tongji-analytics', {
+      key: '194be3ff940b36ed58a65fa8c8c6accb',
+      dev: true,
+      open: true
+    }]
   ],
   extendMarkdown: (md) => {
     md.set({ breaks: true });
