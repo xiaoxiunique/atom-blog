428b217e0bb6deef57eca2f30b9642fd73278955
xiaoxiunique@gmail.com <xiaoxiunique@gmail.com>
2020-10-30 18:34:49 +0800

    feat: add vuetify

--- gitee.sh
+++ gitee.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd .vuepress/dist
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@gitee.com:xiaoxiunique/xiaoxi.gitee.io.git master
+git push -f git@gitee.com:xiaoxiunique/atom-blog-html.git master
 
 cd ../../
 
